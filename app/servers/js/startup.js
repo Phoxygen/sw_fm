adb38c423bd5e8d870d5ae72dcdbd0d13c318890
Augustin Trancart <augustin.trancart@phoxygen.com>
2015-08-04 15:56:08 +0200

    First pass at removing server abstraction

--- app/servers/js/startup.js
+++ app/servers/js/startup.js
@@ -1,4 +1,71 @@
 'use strict';
+function sendToSmuggler(server, command) {
+  var smuggler = new BroadcastChannel('smuggler');
+  smuggler.postMessage({
+    name: command,
+    type: 'server',
+  });
+  smuggler.close();
+}
 
-var s1 = new Server('logic');
+function debug() {
+  console.log.bind(console, '[server]').apply(console, arguments);
+}
+
+function createServer() {
+
+  function Server() {
+    this.listen();
+    // the server register itself when it is ready
+    this.port = new BroadcastChannel('logic');
+
+    // we keep a ref to the listener to be able to remove it.
+    this.port.onMessageListener = e => {this.onmessage.call(this, this.port, e.data);};
+    this.port.addEventListener(
+      'message',
+      this.port.onMessageListener
+    );
+  }
+
+  Server.prototype.onglobalmessage = function(data) {
+
+    if (data.type === 'unregister') {
+      this.unregisterClient();
+    }
+  };
+
+  Server.prototype.unregisterClient = function() {
+    debug('Unregistering client');
+
+    this.port.removeEventListener('message', this.port.onMessageListener);
+    this.port.postMessage({ type: 'disconnected', });
+    this.port.close();
+    this.port = null;
+    // don't accept new clients
+    this.unlisten();
+    // tell the smuggler we are useless
+    this.unregister();
+  };
+
+  Server.prototype.listen = function() {
+    this.onglobalmessageListener = e => this.onglobalmessage(e.data);
+    addEventListener('message', this.onglobalmessageListener);
+  };
+
+  Server.prototype.unlisten = function() {
+    removeEventListener('message', this.onglobalmessageListener);
+    this.onglobalmessageListener = null;
+  };
+
+  Server.prototype.unregister = function() {
+    debug('Unregistering server ');
+    sendToSmuggler(this, 'unregister');
+  };
+
+  var server = new Server();
+
+  return server;
+}
+
+var s1 = createServer();
 
